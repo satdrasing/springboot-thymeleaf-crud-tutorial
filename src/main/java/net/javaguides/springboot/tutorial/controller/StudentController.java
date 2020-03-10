c1f37f4f95b53805739b47409bd0fc2b20715593
satdrasing <satendra.kohli@gmail.com>
2020-03-10 19:25:59 +0530

    security added

--- src/main/java/net/javaguides/springboot/tutorial/controller/StudentController.java
+++ src/main/java/net/javaguides/springboot/tutorial/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,7 +68,7 @@ public class StudentController {
 		return "index";
 	}
 
-	@GetMapping("delete/{id}")
+	@PostMapping("delete/{id}")
 	public String deleteStudent(@PathVariable("id") long id, Model model) {
 		Student student = studentRepository.findById(id)
 				.orElseThrow(() -> new IllegalArgumentException("Invalid student Id:" + id));
