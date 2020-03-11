e2859e4d11ceb59db0d94205c8c147158f5ca7b7
satdrasing <satendra.kohli@gmail.com>
2020-03-11 16:43:49 +0530

    removed boilerplate code

--- src/main/java/net/javaguides/springboot/tutorial/entity/Student.java
+++ src/main/java/net/javaguides/springboot/tutorial/entity/Student.java
@@ -1,6 +1,10 @@
 package net.javaguides.springboot.tutorial.entity;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,59 +13,24 @@ import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    
+
     @NotBlank(message = "Name is mandatory")
     @Column(name = "name")
     private String name;
-    
+
     @NotBlank(message = "Email is mandatory")
     @Column(name = "email")
     private String email;
 
     @Column(name = "phone_no")
     private long phoneNo;
-    
-    public Student() {}
 
-    public Student(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-    
-    public long getId() {
-        return id;
-    }
-    
-    public void setName(String name) {
-        this.name = name;
-    }
-    
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-	public long getPhoneNo() {
-		return phoneNo;
-	}
-
-	public void setPhoneNo(long phoneNo) {
-		this.phoneNo = phoneNo;
-	}
 }
